195e872d333f6dafaba5e18ba0b6fe221ca36ec7
Hope Tambala <hopetambala@outlook.com>
2020-03-26 13:32:57 -0400

    feat: add endpoints for cart quantity
    @chance-murphy check the cart.py in endpoints and check to see if this makes sense

--- endpoints/cart.py
+++ endpoints/cart.py
@@ -19,102 +19,50 @@ class Cart(Resource):
             }, 200
         else:
             return {'message': 'Cart not found!'}, 404
+    
+    def post(self):
+        parser = reqparse.RequestParser()
 
+        parser.add_argument('user_id',
+                            type=int,
+                            required=True,
+                            help='This field is mandatory!')
+        parser.add_argument('product_upc',
+                            type=str,
+                            required=True,
+                            help='This field is mandatory!')
+        parser.add_argument('quantity',
+                            type=int,
+                            required=True,
+                            help='This field is mandatory!')
+        parser.add_argument('type',
+                            type=str,
+                            required=True,
+                            help='This field is mandatory!')
 
-# class CartItem(Resource):
-#     def post(self, user_id, cart_id):
+        data_payload = parser.parse_args()
+
+        if data_payload['type'] == 'add_product_for_user':
+            CartModel.add_product_for_user(data_payload['user_id'],
+                                data_payload['product_upc'],
+                                data_payload['quantity'])
+            return {'message': 'Product successfully added to database!'}, 201
         
-#         parser = reqparse.RequestParser()
-#         parser.add_argument('user_id',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
+        if data_payload['type'] == 'remove_product_for_user':
+            CartModel.remove_product_for_user(data_payload['user_id'],
+                                data_payload['product_upc'],
+                                data_payload['quantity'])
+            return {'message': 'Product successfully removed from database!'}, 201
+        if data_payload['type'] == 'increment_product_amt_for_user':
+            CartModel.increment_product_amt_for_user(data_payload['user_id'],
+                                data_payload['product_upc'],
+                                data_payload['quantity'])
+            return {'message': 'Product successfully updated in database!'}, 201
+        if data_payload['type'] == 'decrement_product_amt_for_user':
+            CartModel.decrement_product_amt_for_user(data_payload['user_id'],
+                                data_payload['product_upc'],
+                                data_payload['quanity'])
+            return {'message': 'Product successfully updated in database!'}, 201
 
-#         parser.add_argument('product_upc',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
-        
-#         parser.add_argument('quantity',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
-
-#         data_payload = parser.parse_args()
-#         cart = CartModel.retrieve_cart_by_user_id(data_payload['user_id'])
-
-#         if (data_payload['type'] =='add_product'):
-#             cart.add_product(data_payload['product_upc'], data_payload['quanity'])
-#             return {'message': 'Cart Item successfully added to database!'}, 201
-            
-#         if (data_payload['type'] =='remove_product'):
-#             cart.remove_product(data_payload['product_upc'])
-#             return {'message': 'Cart Item successfully removed from database!'}, 201
-
-
-# class CartItemQuanity(Resource):
-#     def post(self, user_id, cart_id, product_id):
-        
-#         parser = reqparse.RequestParser()
-#         parser.add_argument('cart_id',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
-
-#         parser.add_argument('product_id',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
-        
-#         parser.add_argument('type',
-#                             type=str,
-#                             required=True,
-#                             help='This field is mandatory!')
-        
-#         parser.add_argument('quantity',
-#                             type=int,
-#                             required=True,
-#                             help='This field is mandatory!')
-
-#         data_payload = parser.parse_args()
-
-#         cart = CartModel.retrieve_cart_by_user_id(data_payload['user_id'])
-
-#         if (data_payload['type'] =='increment'):
-#             cart.increment_product_amt(data_payload['product_id'])
-            
-#         if (data_payload['type'] =='decrement'):
-#             cart.increment_product_amt(data_payload['product_id'])
-        
-#         return {'message': 'Cart Item Quanitity successfully updated in database!'}, 201
-
-
-# class InventoryProductList(Resource):
-
-#     def get(self):
-#         products = InventoryModel.find_all_products();
-#         if products:
-#             return {'products': [product.json() for product in products]}, 200
-#         else:
-#             return {'message': 'No products found!'}, 404
-
-
-# class Shopping(Resource):
-
-#     def post(self):
-#         parser = reqparse.RequestParser()
-
-#         parser.add_argument('username',
-#                             type=str,
-#                             required=True,
-#                             help='This field is mandatory!')
-
-#         parser.add_argument('product',
-#                             type=str,
-#                             required=True,
-#                             help='This field is mandatory!')
-
-#         data_payload = parser.parse_args()
-
-#         return ShoppingInventory.buy_product(data_payload['username'],
-#                                          data_payload['product'])
+        else:
+            return {'message': 'Womp, you typed in the type wrong'}, 201        
